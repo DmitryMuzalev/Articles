fb0ccba9dfc1ce9d1456fe0496262c5e07f1b3dd
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2023-01-22 00:06:58 +0300

    Working tabs

--- src/components/navigation.component.js
+++ src/components/navigation.component.js
@@ -3,9 +3,14 @@ import { Component } from '../core/component';
 export class NavigationComponent extends Component {
   constructor(id) {
     super(id);
+    this.tabs = [];
   }
   init() {
     this.$el.addEventListener('click', tabClickHandler.bind(this));
+  }
+
+  registerTabs(tabs) {
+    this.tabs = tabs;
   }
 }
 
@@ -15,6 +20,13 @@ function tabClickHandler(event) {
     Array.from(this.$el.querySelectorAll('.tab')).forEach((tab) => {
       tab.classList.remove('active');
     });
+    this.tabs.forEach((t) => {
+      t.component.hide();
+    });
+
     event.target.classList.add('active');
+    this.tabs
+      .find((t) => t.name === event.target.dataset.name)
+      .component.show();
   }
 }
